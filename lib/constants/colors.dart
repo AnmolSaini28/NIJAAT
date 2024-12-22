5c3f440560d467fc8aa6d5ec6ab396c87e41abdf
AnmolSaini28 <sainianmol1507@gmail.com>
2024-12-22 17:51:41 +0530

    Branch Created

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const secondaryColor = Color(0xffFDFCF5);
+Color primaryColor = Color(0xff018749).withOpacity(0.7);
+const textColor = Color(0xff234850);
+const subTextColor = Color(0xff888784);
+const homeTile1 = Color(0xff972AA7);
+const homeTile2 = Color(0xff2293E6);
+const homeTile3 = Color(0xff4BAB4B);
+const homeTile4 = Color(0xffE0215D);
+const sos = Color(0xffF44336);
+const bottomIcons = Color(0xff8D8C88);
+const bottomText = Color(0xff000000);
+const headingColor = Color(0xff234850);
+const textFieldColor = Color(0xFFD9D9D9);
